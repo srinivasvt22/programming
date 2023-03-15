1cb05e2b524addf2fd6c1b6f7c881f13ad64ddae
srinivasvt22 <121662205+srinivasvt22@users.noreply.github.com>
2023-03-14 23:39:21 -0700

    Update BuySellStock.java

--- Array/BuySellStock.java
+++ Array/BuySellStock.java
@@ -1,15 +1,14 @@
 private static int minBuyMaxSell(int[] prices) {
-		//Initialize
-		int max=0,min=Integer.MAX_VALUE;
+   //Initialize
+   int max=0,min=Integer.MAX_VALUE;
 		
-		//Iterate an array	
-		for(int start=0;start<prices.length;start++) {
-			
-      //find Min & Max
-			min=Math.min(prices[start], min);
-			max=Math.max(prices[start]-min, max);
-				
-        }
+  //Iterate an array	
+  for(int start=0;start<prices.length;start++) {
+  //calculate Min & Max
+  min=Math.min(prices[start], min);
+  max=Math.max(prices[start]-min, max);
+
+   }
 		
-		return max;
-	}
+return max;
+}
