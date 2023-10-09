bb3d6983d9d10a1493b7a11da95de50396a26a59
srinivasvt22 <121662205+srinivasvt22@users.noreply.github.com>
2023-10-08 23:34:01 -0700

    Update ContainerWithMostWater.java

--- Array/ContainerWithMostWater.java
+++ Array/ContainerWithMostWater.java
@@ -1,1 +1,16 @@
-
+//With Two Pointer approach for linear solution O(n)
+	private static int maxAreaTwoPointers(int[] height) {
+		int maxArea=0,left=0,right=height.length-1;
+		
+		while(left<right) {
+			
+			int area=(right-left) * Math.min(height[left], height[right]);
+			maxArea=Math.max(maxArea, area);
+			
+			if(height[left]<height[right]) left++;
+			else right--;
+		}
+		
+		
+		return maxArea;
+	}
